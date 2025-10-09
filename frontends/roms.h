667f01806b93b194404bbeec49a38499d5cd6d1c
Ben <benjsouthwell@gmail.com>
2025-10-09 17:33:33 +1100

    add rom file to frontend

---
+++ frontends/roms.h
@@ -0,0 +1,69 @@
+#ifndef ROMS_H
+#define ROMS_H
+
+/*
+A simple header-only library that reads ROMs from disk
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+
+#define MAX_ROM_SIZE (3584) /* 4096 - 512 (0x200) */
+
+struct rom {
+    uint8_t * data;
+    uint16_t num_bytes;
+};
+
+static inline struct rom *
+read_rom(const char * path)
+{	
+    FILE    *infile;
+    long    numbytes;
+    struct rom * r;
+
+    infile = fopen(path, "r");
+ 
+    if(infile == NULL)
+    {
+        fprintf(stderr, "could not open: %s\n", path);
+        return NULL;
+    }
+
+    fseek(infile, 0L, SEEK_END);
+    numbytes = ftell(infile);
+
+    if (numbytes > MAX_ROM_SIZE)
+    {
+        fprintf(stderr, "%s is %ld, maximum size is MAX_ROM_SIZE\n", path, numbytes);
+    }
+
+    fseek(infile, 0L, SEEK_SET);
+
+    r = malloc(sizeof(struct rom));
+    r->data = malloc(numbytes);
+    r->num_bytes = numbytes;
+
+    fread(r->data, sizeof(uint8_t), numbytes, infile);
+    fclose(infile);
+
+    return r;
+}
+
+static inline void 
+free_rom(struct rom * r)
+{
+    if (r == NULL)
+        return;
+
+    if (r->data != NULL)
+        free(r->data);
+    
+    free(r);
+}
+
+#undef MAX_ROM_SIZE
+
+#endif // ROMS_H

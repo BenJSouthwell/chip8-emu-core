f6bbb1e87cd3f2679ca887c20d19e609bc3d4102
Ben <benjsouthwell@gmail.com>
2025-10-14 23:03:17 +1100

    use ncurses for the frontend

--- frontends/main.c
+++ frontends/main.c
@@ -2,11 +2,14 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#include <ncurses.h> 
+# include "locale.h"
+
 #include "chip8.h"
 #include "roms.h"
 
-/* a janky (TODO unjank it) renderer */
-static void render_to_terminal(struct chip8_io * chip8_io);
+static void draw_basic_display(struct chip8_io *io, int start_x, int start_y);
+static int get_chip8_key(int ch);
 
 int 
 main(int argc, char* argv[])
@@ -14,75 +17,109 @@ main(int argc, char* argv[])
     struct chip8 * p;
     struct chip8_io * chip8_io;
     struct rom * r;
-    int i;
-    
+    int i, ch, key, k;
+    useconds_t sleep_time;
+
+    sleep_time = 1667; /* 600 Hz */
+
+    initscr(); 
+    cbreak();   
+    noecho();  
+    curs_set(0);
+    nodelay(stdscr, TRUE);
+
+    attron(A_BOLD);
+    mvprintw(0, 0, "CHIP-8 Emulator - 600Hz CPU. Press ESC to quit.");
+    attroff(A_BOLD);
+    setlocale(LC_ALL, "en-US.UTF-8");
+
     if (argc != 2)
     {
         fprintf(stderr, "usage:\n\t%s <ROM_FILE>\n", argv[0]);
         exit(1);
     }
     
-    printf("loading rom file: %s\n", argv[1]);
-
-
     p = initialise_chip8(CHIP8_CLOCK_RATE_600Hz);
     chip8_io = get_io_chip8(p);
     r = read_rom(argv[1]);
     load_rom_chip8(p, r->data, r->num_bytes);
 
-    for(i=0; i<2000; i ++)
+    for(;;)
     {
-        // Non-blocking keyboard input
-        fd_set set;
-        struct timeval timeout = {0, 0};
-        FD_ZERO(&set);
-        FD_SET(STDIN_FILENO, &set);
-        int rv = select(STDIN_FILENO + 1, &set, NULL, NULL, &timeout);
-        if (rv > 0) {
-            char ch;
-            read(STDIN_FILENO, &ch, 1);
-            // Process input character 'ch' as needed
+        ch = getch();
+        
+        if (ch != ERR) {
+            // map the key to the CHIP8 Keypad
+            key = get_chip8_key(ch) ;
+            if (key >= 0)
+            {
+                chip8_io->keypad_state[key] = 1;
+            }
+            else if (ch == 27)
+            {
+                /*escape key pressed */
+                break; 
+            }     
         }
 
-        // Fetch, decode and execute an instruction
+        /* Fetch, decode and execute an instruction */
         execute_cycle_chip8(p);
 
-        // draw to screen 
         if(chip8_io->update_display)
         {
-            render_to_terminal(chip8_io);
+            draw_basic_display(chip8_io, 1, 2);
+            refresh(); 
+            memset(chip8_io->keypad_state, 0, 16*sizeof(uint8_t));
+        }
+  
+        if(chip8_io->buzzer_active)
+        {
+            beep();
         }
         
-        // update inputs
-
-        // throttle the loop and wait a bit
-        usleep(1667); // ~600 FPS
+        usleep(sleep_time); 
     }
  
+    endwin(); 
+    printf("Thanks for playing ...\n");
+    fflush(stdout);
     return 0;
 }
 
-
-void 
-render_to_terminal(struct chip8_io * chip8_out)
-{
-    int r,c;
-    /* Move cursor to top-left and clear screen */
-    printf("\033[H\033[J");
-    for( c =0; c < CHIP8_SCREEN_WIDTH; c++)
-        printf("-");
-    printf("\n");
-    for( r=0; r < CHIP8_SCREEN_HEIGHT; r++)
-    {
-        for( c =0; c < CHIP8_SCREEN_WIDTH; c++)
-        {
-            if (chip8_out->fbuff[c + r * CHIP8_SCREEN_WIDTH])
-                printf("*");
-            else
-                printf(" ");
+static
+void
+draw_basic_display(struct chip8_io *io, int start_x, int start_y) {
+    int x, y, pixel;    
+    for (y = 0; y < CHIP8_SCREEN_HEIGHT; y++) {
+        for (x = 0; x < CHIP8_SCREEN_WIDTH; x++) {
+            pixel = io->fbuff[y * CHIP8_SCREEN_WIDTH + x];
+            mvaddch(start_y + y, start_x + x*2, pixel ? (char)219  : ' ');
+            mvaddch(start_y + y, start_x + x*2 +1, pixel ? (char)219 : ' ');
         }
-        printf("\n");
     }
-    fflush(stdout); // Ensure output is shown immediately
+}
+
+static  
+int
+get_chip8_key(int ch) {
+    switch(ch) {
+        case '1': return 0x1;
+        case '2': return 0x2;
+        case '3': return 0x3;
+        case '4': return 0xC;
+        case 'q': case 'Q': return 0x4;
+        case 'w': case 'W': return 0x5;
+        case 'e': case 'E': return 0x6;
+        case 'r': case 'R': return 0xD;
+        case 'a': case 'A': return 0x7;
+        case 's': case 'S': return 0x8;
+        case 'd': case 'D': return 0x9;
+        case 'f': case 'F': return 0xE;
+        case 'z': case 'Z': return 0xA;
+        case 'x': case 'X': return 0x0;
+        case 'c': case 'C': return 0xB;
+        case 'v': case 'V': return 0xF;
+        default: return -1;
+    }
 }
  
